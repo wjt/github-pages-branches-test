bae7b8de91485c1ef736bc5da1ee6f08cc4c67c7
Will Thompson <wjt@endlessos.org>
2025-02-14 14:52:13 +0000

    eh

--- godoctopus.py
+++ godoctopus.py
@@ -34,6 +34,7 @@ def main():
 
     branch_artifacts = {}
     # TODO: pagination
+    # TODO: sanitize branch names
     for artifact in response.json()["artifacts"]:
         if artifact["workflow_run"]["repository_id"] != artifact["workflow_run"]["head_repository_id"]:
             # TODO: external PRs
@@ -46,6 +47,9 @@ def main():
         branch_artifacts.setdefault(head_branch, {}).setdefault(name, []).append(artifact)
 
     tmpdir = pathlib.Path(tempfile.mkdtemp(prefix="godoctopus-"))
+    branches_dir = tmpdir / "branches"
+    branches_dir.mkdir()
+
     logging.info("Assembling site at %s", tmpdir)
     url = branch_artifacts["main"]["web"][0]["archive_download_url"]
     logging.info("Fetching main branch export from %s", url)
@@ -68,7 +72,7 @@ def main():
                 shutil.copyfileobj(response.raw, f)
                 zip = zipfile.ZipFile(f)
                 pck = zip.open("index.pck", "r")
-                with (tmpdir / "branches" / f"{branch}.pck").open("wb") as target:
+                with (branches_dir / f"{branch}.pck").open("wb") as target:
                     shutil.copyfileobj(pck, target)
 
     github_output = os.environ.get("GITHUB_OUTPUT")
